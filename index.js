571db69b1eebef4cb4304246ca7f62914879bf0c
ViniciusBarbosa <vinicius.barbosa2@outlook.com>
2024-01-05 23:50:56 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+class hero{
+    constructor(nome, idade, tipo[guerreiro, mago, monge, ninja])
+    this.nome = nome
+    this.idade = idade
+    this.tipo = tipo[]
+}
+
+let ataque = ["espada", "magia", "artes marciais", "shuriken"]
+
+    atacar () {
+        console.log(`O ${this.tipo[]} atacou usando ${ataque}`)
+    }
+
