808a1db5ef05b74b6f3733976900f136ee77ae29
ViniciusBarbosa <vinicius.barbosa2@outlook.com>
2024-01-06 13:36:33 -0300

    testado e finalizado

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 class hero{
-    constructor(nome, idade, tipo[guerreiro, mago, monge, ninja])
-    this.nome = nome
-    this.idade = idade
-    this.tipo = tipo[]
-}
-
-let ataque = ["espada", "magia", "artes marciais", "shuriken"]
-
-    atacar () {
-        console.log(`O ${this.tipo[]} atacou usando ${ataque}`)
+    constructor(nome, idade, tipo){
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
     }
 
+    atacar () {
+        let ataque
+        if (this.tipo == "guerreiro") {
+            ataque = "espada"
+        } else if (this.tipo == "mago") {
+            ataque = "magia"
+        } else if (this.tipo == "monge") {
+            ataque = "artes marciais"
+        } else {
+            ataque = "shuriken"
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+let heroiVinicius = new hero("Harry", 33, "guerreiro")
+let heroiThamires = new hero("Hermione", 30, "mago")
+
+heroiVinicius.atacar();
+heroiThamires.atacar();
